15bdacf5b24f0a58a004a5c24a3f2afab9327537
Gerben PC <gerben.01@hotmail.nl>
2026-01-11 10:58:56 +0100

    Update stats

--- simulation_part/constants.py
+++ simulation_part/constants.py
@@ -1,5 +1,5 @@
 # Settings
-PRINT_SIM_PROGRESS = True
+PRINT_SIM_PROGRESS = False
 
 
 # Time values
@@ -10,9 +10,9 @@ DURATION_HOURS = HOURS_IN_DAY*DURATION_DAYS
 
 # Data generation process
 SEED = 37
-PATIENT_DISTRIBUTIONS = [(('exponential', (HOURS_IN_DAY/17,)),  # Arrival time patient type I
+PATIENT_DISTRIBUTIONS = [(('exponential', (HOURS_IN_DAY/16.95,)),  # Arrival time patient type I
                           ('normal', (25.71/60, 5.84/60))),  # Scan duration type I
-                         (('exponential', (HOURS_IN_DAY/10,)),  # Arrival time patient type II
+                         (('exponential', (HOURS_IN_DAY/9.95,)),  # Arrival time patient type II
                           ('lognormal', (40.71/60, 12.07/60)))]  # Scan duration type II
 NUM_MACHINES = 2
 
